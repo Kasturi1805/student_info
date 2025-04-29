6cfd860b1512b345d7bcb553ff2ee9caa97d933a
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-29 12:08:19 -0500

    save

--- Strudent_info.java
+++ Strudent_info.java
@@ -1,5 +1,27 @@
-package Student_info;
 
 public class Strudent_info {
+    String name,ID,Course;
+
+   
+
+    public Strudent_info(String name, String iD, String course) {
+        this.name = name;
+        ID = iD;
+        Course = course;
+    }
+
+
+
+    public void displayInfo(){
+        System.out.println("Student name:"+name);
+        System.out.println("Student id:"+ID);
+        System.out.println("Student Course:" +Course);
+
+    }
+
+    public static void main(String[] args) {
+        Strudent_info s = new Strudent_info("Anu", "01fe23bca248", "BCA");
+    }
 
 }
+
