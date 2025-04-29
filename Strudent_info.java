817f8f4af67345fdc70664cf715d2492baca7c53
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-29 12:16:43 -0500

    save

--- Strudent_info.java
+++ Strudent_info.java
@@ -1,13 +1,14 @@
 
 public class Strudent_info {
-    String name,ID,Course;
-
+    String name,ID,Course,Address,Mobile_no;
    
 
-    public Strudent_info(String name, String iD, String course) {
+    public Strudent_info(String name, String iD, String course,String Address,String Mobile_no) {
         this.name = name;
         ID = iD;
         Course = course;
+        Address = Address;
+        Mobile_no = Mobile_no;
     }
 
 
@@ -16,11 +17,13 @@ public class Strudent_info {
         System.out.println("Student name:"+name);
         System.out.println("Student id:"+ID);
         System.out.println("Student Course:" +Course);
+        System.out.println("Student Address:" +Address);
+        System.out.println("Student Mobile_no:" +Mobile_no);
 
     }
 
     public static void main(String[] args) {
-        Strudent_info s = new Strudent_info("Anu", "01fe23bca248", "BCA");
+        Strudent_info s = new Strudent_info("Anu", "01fe23bca248", "BCA","Delhi","8123456789");
     }
 
 }
